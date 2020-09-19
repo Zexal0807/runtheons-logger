c36cb5a97e21d4bb8ecb9a958a326d2a5e117085
Zexal0807 <gallinar00@gmail.com>
2020-09-19 13:17:45 +0200

    Implement log for user

--- index.js
+++ index.js
@@ -1,26 +1,35 @@
 const fs = require('fs');
 const util = require('util');
 
-module.exports = new class logger{
-	
-	sql(sql, err, result, fields){
-		var path = 'log/sql/' + new Date().toISOString().slice(0,10) + '.txt',
-		time = new Date().toTimeString().slice(0,8);
-		fs.open(path, 'a', function(e, file) {
-			if (e)
-				throw e;
-			
-			var str = util.inspect(result);
-			str = str.replace(/\n/g, "\n\t");
-			str = '[' + time + '] : ' + sql + '\nResult:\n\t'+str+'\n\r';
-			fs.write(file, str, function(e) {
-				if (e)
-					throw err;
-				fs.close(file, function(){
-					
-				});
-			});
-		});
-	}
-	
+module.exports = new class logger {
+
+    sql(sql, result, user = undefined) {
+        var path = 'log/sql/';
+        if (user != undefined) {
+            path += user + '/';
+
+        }
+
+        if (!fs.existsSync(path)) {
+            fs.mkdirSync(path);
+        }
+        path += new Date().toISOString().slice(0, 10) + '.txt';
+
+        var time = new Date().toTimeString().slice(0, 8);
+        fs.open(path, 'a', function(e, file) {
+            if (e)
+                throw e;
+
+            var str = util.inspect(result);
+            str = str.replace(/\n/g, "\n\t");
+            str = '[' + time + '] : ' + sql + '\nResult:\n\t' + str + '\n\r';
+
+            fs.write(file, str, function(er) {
+                if (er)
+                    throw er;
+                fs.close(file, function() {});
+            });
+        });
+    }
+
 };
